484f12b25e2c901340310c7db693d3fc58395da8
ryanrkt <ryanrakoto10@gmail.com>
2026-02-09 16:03:07 +0300

    Base de donnee

---
+++ database/test.sql
@@ -0,0 +1,5 @@
+INSERT INTO ROLE (libelle) VALUES ('Admin'),('Client');
+
+
+INSERT INTO UTILISATEUR(nom,prenom,email,mot_de_passe,role_id) VALUES ('admin','admin','admin@gmail.com','admin123',1);
+
